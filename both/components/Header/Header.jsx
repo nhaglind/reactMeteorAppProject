6346c9fd917c5039e5d1b2d5e8e4687cd4bafce4
nhaglind <nhaglind@gmail.com>
2016-04-08 15:16:37 -0500

    change nav on login

--- both/components/Header/Header.jsx
+++ both/components/Header/Header.jsx
@@ -4,14 +4,26 @@ Header = React.createClass({
       backgroundColor: "#e1bee7",
       paddingLeft: "12px"
     };
+    var navOptions;
+    if (User.isLoggedIn()) {
+      navOptions = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="/">Home</a></li>
+        </ul>
+      );
+    } else {
+      navOptions = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="/register">Sign Up</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+      );
+    }
     return (
       <nav style={navStyle}>
         <div className="nav-wrapper">
           <a href="/" className="brand-logo">Kosupure</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/register">Sign Up</a></li>
-            <li><a href="/login">Login</a></li>
-          </ul>
+          {navOptions}
         </div>
       </nav>
     );
